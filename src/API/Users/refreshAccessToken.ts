73010e052159d9838dfa00ab9088efaa8cb7299d
Harvmaster <hzuccon@gmail.com>
2024-04-16 13:04:26 +1000

    Created refreshAccessToken.ts which sends a POST request to retrieve a new access token when given a refresh token

---
+++ src/API/Users/refreshAccessToken.ts
@@ -0,0 +1,25 @@
+import api from "../api";
+
+import { AccessTokenResponse } from "../types";
+
+export const refreshAccessToken = async (refresh_token: string) => {
+  try {
+    const data = await api.post<AccessTokenResponse>('/user/refreshAccessToken', {
+      refresh_token
+    })
+
+    return data
+  } catch (err: any) {
+    console.log(err)
+    if (err?.response?.status === 400) {
+      throw new Error(err.response.data.error)
+    }
+    if (err?.response?.status === 401) {
+      throw new Error(err.response.data.error)
+    }
+
+    throw new Error(err)
+  }
+}
+
+export default refreshAccessToken
