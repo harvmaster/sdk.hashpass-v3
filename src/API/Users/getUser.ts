5023d7a26192904c64df85e9370eeb70c01d3bee
Harvmaster <hzuccon@gmail.com>
2024-04-16 13:03:20 +1000

    Created getUser.ts which will get the user information based on the Authorization header token

---
+++ src/API/Users/getUser.ts
@@ -0,0 +1,31 @@
+import api, { RequestOptions } from "../api";
+
+import { User } from "../types";
+
+export const getUser = async (access_token: string) => {
+  const headers: RequestOptions['headers'] = {}
+  if (access_token) headers['Authorization'] = `Bearer ${access_token}`
+
+  try {
+    const data = await api.get<User>('/user', {
+      headers
+    })
+
+    return data
+  } catch (err: any) {
+    console.log(err)
+    if (err?.response?.status === 400) {
+      throw new Error(err.response.data.error)
+    }
+    if (err?.response?.status === 401) {
+      throw new Error(err.response.data.error)
+    }
+    if (err?.response?.status === 404) {
+      throw new Error(err.response.data.error)
+    }
+
+    throw new Error(err)
+  }
+}
+
+export default getUser
