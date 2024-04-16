e98a3664e8a75fc4d73192402099fb5231ba3ead
Harvmaster <hzuccon@gmail.com>
2024-04-16 13:02:47 +1000

    Created createUser.ts which creates a user

---
+++ src/API/Users/createUser.ts
@@ -0,0 +1,39 @@
+import AuthToken from "../Auth/AuthToken";
+import api from "../api";
+
+import { UserAuthResponse } from "../types";
+
+/**
+ * Sends post request to creeate a user.
+ * Does not hash or encrypt the password. Make sure you do this before calling this method
+ * @param username 
+ * @param password 
+ * @returns 
+ */
+export const createUser = async (username: string, password: string) => {
+  try {
+    const data = await api.post<UserAuthResponse>('/user', {
+      username,
+      password
+    })
+
+    const authToken = new AuthToken(data)
+
+    return {
+      user: data.user,
+      authToken
+    }
+  } catch (err: any) {
+    console.log(err)
+    if (err?.response?.status === 400) {
+      throw new Error(err.response.data.error)
+    }
+    if (err?.response?.status === 409) {
+      throw new Error(err.response.data.error)
+    }
+
+    throw new Error(err)
+  }
+}
+
+export default createUser
