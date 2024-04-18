58e990f28260f149d34e048b37af7d73dbed55bb
Harvmaster <hzuccon@gmail.com>
2024-04-18 19:00:56 +1000

    Added test for unequal length in hexToBase58

--- src/Crypto/Encodings/Encodings.test.ts
+++ src/Crypto/Encodings/Encodings.test.ts
@@ -5,4 +5,8 @@ describe('hexToBase58', () => {
     const base58 = hexToBase58('b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9')
     expect(base58).toBe('DULfJyE3WQqNxy3ymuhAChyNR3yufT88pmqvAazKFMG4')
   })
+
+  test('should throw an error if the hex string has an odd length', () => {
+    expect(() => hexToBase58('b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde')).toThrow('Hex string has an odd length!')
+  })
 })
