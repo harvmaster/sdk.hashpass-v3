9441992f3a72c185426b94534fc445c4ba95c045
Harvmaster <hzuccon@gmail.com>
2024-04-18 18:19:10 +1000

    Made getGenerator have uppercased options in the algorithms object

--- src/Crypto/PasswordGenerators/getGenerator.ts
+++ src/Crypto/PasswordGenerators/getGenerator.ts
@@ -6,13 +6,13 @@ type PasswordGenerator = (secret: string, service: string) => string
 type Algorithm = 'LEGACY' | 'HEX' | 'BASE58'
 
 export const algorithms: Record<string, PasswordGenerator> = {
-  legacy: generateLegacyPassword,
-  hex: generateHexPassword,
-  base58: generateBase58Password,
+  LEGACY: generateLegacyPassword,
+  HEX: generateHexPassword,
+  BASE58: generateBase58Password,
 }
 
 export const getGenerator = <T extends string>(algorithm: Uppercase<T> extends Algorithm ? T : Algorithm): PasswordGenerator  => {
-  return algorithms[algorithm.toLocaleLowerCase()];
+  return algorithms[algorithm.toLocaleUpperCase()];
 }
 
 export default getGenerator
